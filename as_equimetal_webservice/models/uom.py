1626047e815d3b3d5ad01391604b8e759f4761fd
Antonio Ruban <antoniodavid8@gmail.com>
2021-12-09 14:48:41 -0500

    [REF] Change unidad_sap

--- as_equimetal_webservice/models/uom.py
+++ as_equimetal_webservice/models/uom.py
@@ -4,5 +4,5 @@ from odoo import fields, models, api
 class Uom(models.Model):
     _inherit = 'uom.uom'
 
-    unidad_sap = fields.Selection(selection=[('BI', 'BI'), ('BO', 'BO')])
+    unidad_sap = fields.Char()
     as_contenido_envase = fields.Char()
