40cf1e9ef8115f9cd069a65435a0c8c03514a72c
Antonio Ruban <antoniodavid8@gmail.com>
2021-12-20 09:38:57 -0500

    [ADD] Migrations

---
+++ as_equimetal_webservice/migrations/0.0.0/pre-uom_unidad_sap.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    _logger.info("PRE Update --> {}".format(version))
+    query = """ALTER TABLE uom_uom DROP COLUMN IF EXISTS unidad_sap;"""
+    cr.execute(query)
