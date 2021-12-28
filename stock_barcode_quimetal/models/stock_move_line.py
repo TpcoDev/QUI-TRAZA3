1f87c7b9d950ff61c8117c6fad94e2226b2157af
Antonio Ruban <antoniodavid8@gmail.com>
2021-12-28 14:17:36 -0500

    [REF] cambiando el nombre

--- stock_barcode_quimetal/models/stock_move_line.py
+++ stock_barcode_quimetal/models/stock_move_line.py
@@ -89,5 +89,5 @@ class StockMoveLine(models.Model):
             b64_pdf = base64.b64encode(pdf[0])
             content_bytes = base64.b64decode(b64_pdf, validate=True)
             dt_string = datetime.now().strftime("%d-%m-%Y %H:%M:%S").split(' ')
-            filename = f"{self.display_name} {dt_string[0]} {dt_string[1]}.pdf"
+            filename = f"{self.lot_name}-{self.product_id.default_code}-{dt_string[0]}-{dt_string[1]}.pdf"
             self.env['google.drive.config'].sudo().upload_pdf(filename, content_bytes)
