7419792efae9d481efc15d3070e5ee1dc1de1fc2
Antonio Ruban <antoniodavid8@gmail.com>
2021-12-28 13:43:03 -0500

    [FIX] Weeeror

--- as_equimetal_webservice/__manifest__.py
+++ as_equimetal_webservice/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Ahorasoft EQUIMETAL customizaciones",
-    'version': "1.4.20211228",
+    'version': "1.4.20211228.1",
     'author': "Ahorasoft",
     'description': """
 Webservice dummy equimetal
